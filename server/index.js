1f8ca39b1556724b6abc6a27f71e91c4d8abae1c
Vartika Rana <vartikarana2000@gmail.com>
2021-06-21 23:10:53 +0530

    add start script

--- server/index.js
+++ server/index.js
@@ -72,8 +72,6 @@ app.post('/upload', async (req, res) => {
     }
 })
 
-
-
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/justSellIt';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
