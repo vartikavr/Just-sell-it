01c15afa586c14d903adc68570b9750b003b4683
Vartika Rana <vartikarana2000@gmail.com>
2021-06-22 12:10:49 +0530

    route correction

--- server/index.js
+++ server/index.js
@@ -76,7 +76,7 @@ app.post('/upload', async (req, res) => {
 
 //Express serve up index.html file if it doesn't recognize route
 const path = require('path');
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/my-project/build/index.html'));
 });
 
